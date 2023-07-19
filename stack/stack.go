db36abe56047f4f3be74803bfb73fd61cd49c05d
betandr <bet@andr.io>
2023-07-19 14:05:10 +0100

    Remove linked list export from stack

--- stack/stack.go
+++ stack/stack.go
@@ -7,39 +7,39 @@ import (
 )
 
 type Stack[T any] struct {
-	LinkedList linked_list.LinkedList[T]
+	linkedList linked_list.LinkedList[T]
 }
 
 func new[T any]() Stack[T] {
 	st := Stack[T]{}
-	st.LinkedList = linked_list.LinkedList[T]{}
+	st.linkedList = linked_list.LinkedList[T]{}
 	return st
 }
 
 func (s *Stack[T]) Push(o T) {
 	node := linked_list.Node[T]{}
 	node.Value = o
-	s.LinkedList.AddHead(&node)
+	s.linkedList.AddHead(&node)
 }
 
 func (s *Stack[T]) Peek() (T, error) {
 	var r T
-	if s.LinkedList.Count == 0 {
+	if s.linkedList.Count == 0 {
 		return r, fmt.Errorf("error: cannot peek an empty stack")
 	}
-	return s.LinkedList.Head.Value, nil
+	return s.linkedList.Head.Value, nil
 }
 
 func (s *Stack[T]) Pop() (T, error) {
 	var r T
-	if s.LinkedList.Count == 0 {
+	if s.linkedList.Count == 0 {
 		return r, fmt.Errorf("error: cannot pop from empty stack")
 	}
 	r, err := s.Peek()
 	if err != nil {
 		return r, err
 	}
-	s.LinkedList.RemoveHead()
+	s.linkedList.RemoveHead()
 
 	return r, nil
 }
@@ -48,7 +48,7 @@ func (l *Stack[T]) String() string {
 	var sb strings.Builder
 	fmt.Fprint(&sb, "TOP")
 
-	node := l.LinkedList.Head
+	node := l.linkedList.Head
 	for node != nil {
 		fmt.Fprintf(&sb, "\n%v", node.Value)
 		node = node.Next
