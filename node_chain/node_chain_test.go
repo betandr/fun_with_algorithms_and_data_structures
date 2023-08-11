97ca187ab8381c069331a85aff1aad2a8d48e734
betandr <bet@andr.io>
2023-08-11 10:09:49 +0100

    Add Node Chain move test

--- node_chain/node_chain_test.go
+++ node_chain/node_chain_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-type nv int
+type nv int // so we can add a String() func
 
 func (n nv) String() string {
 	return fmt.Sprintf("%d", n)
@@ -17,6 +17,24 @@ func TestNodeChain(t *testing.T) {
 	head := Node[nv]{Value: 1, Next: &body}
 
 	want := "1 2 3"
+	got := head.String()
+	if want != got {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func TestMove(t *testing.T) {
+
+	tail := Node[nv]{Value: 4}
+	second := Node[nv]{Value: 3, Next: &tail}
+	first := Node[nv]{Value: 2, Next: &second}
+	head := Node[nv]{Value: 1, Next: &first}
+
+	first.Next = &tail
+	second.Next = &first
+	head.Next = &second
+
+	want := "1 3 2 4"
 	got := head.String()
 	if want != got {
 		t.Errorf("got %s, want %s", got, want)
